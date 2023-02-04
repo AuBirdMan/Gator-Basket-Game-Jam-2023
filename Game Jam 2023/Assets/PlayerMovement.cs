a994bcac69f92f6432a3b4326e919c026149e1c7
RatthewThePlagueBringer <matthewgrimaldi111@gmail.com>
2023-02-04 04:40:00 -0500

    the

--- Game Jam 2023/Assets/PlayerMovement.cs
+++ Game Jam 2023/Assets/PlayerMovement.cs
@@ -5,11 +5,18 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
     private Rigidbody2D rb;
+    private BoxCollider2D currentCollider;
+    private bool isLittle;
+
+    
 
     // Start is called before the first frame update
     private void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        isLittle = false;
+        currentCollider = gameObject.transform.GetChild(0).GetComponent<BoxCollider2D>();
+        
     }
 
     // Update is called once per frame
@@ -23,5 +30,22 @@ public class PlayerMovement : MonoBehaviour
             rb.velocity = new Vector2(rb.velocity.x, 14f);
         }
 
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            isLittle = !isLittle;
+            if (isLittle)
+            {
+                gameObject.transform.GetChild(0).gameObject.SetActive(false);
+                gameObject.transform.GetChild(1).gameObject.SetActive(true);
+                currentCollider = gameObject.transform.GetChild(1).GetComponent<BoxCollider2D>();
+            }
+            else
+            {
+                gameObject.transform.GetChild(0).gameObject.SetActive(true);
+                gameObject.transform.GetChild(1).gameObject.SetActive(false);
+                currentCollider = gameObject.transform.GetChild(0).GetComponent<BoxCollider2D>();
+
+            }
+        }
     }
 }
--- Game Jam 2023/Assets/Scripts/imaginaryfriend.cs
+++ Game Jam 2023/Assets/Scripts/imaginaryfriend.cs
@@ -4,19 +4,32 @@ using UnityEngine;
 
 public class imaginaryfriend : MonoBehaviour
 {
+
+    private Rigidbody2D wisp;
+    private Vector2 player_pos;
+    public GameObject player_go;
+
     // Start is called before the first frame update
     void Start(){
 
-    Debug.Log("start");
+     wisp = GetComponent<Rigidbody2D>();
+     wisp.gameObject.SetActive(false);
 
     }
 
     // Update is called once per frame
     void Update(){
 
-    if (Input.GetKeyDown("space")){
-        Debug.Log("space");
+        if (player_go.transform.GetChild(0).gameObject.activeSelf)
+        {
+            wisp.gameObject.SetActive(true);
+        }
+        else 
+        { 
+            wisp.gameObject.SetActive(false);
         }
 
+        player_pos = player_go.transform.position;
+        wisp.position = new Vector2(player_pos.x - 1, player_pos.y + 2);
     }
 }
