41f9a5c28eb7aef7dcdecc7a8fb893565855a941
Michael <michaelgarcia5656@gmail.com>
2023-02-04 17:22:53 -0500

    idk what i did

---
+++ Game Jam 2023/Assets/ButtonConnector.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonConnector : MonoBehaviour
+{
+    public GameObject button;
+    private bool isPressed;
+    // Start is called before the first frame update
+    void Start()
+    {
+        isPressed = !button.transform.GetChild(0).gameObject.activeInHierarchy;
+    }
+
+    // Update is called once per frame
+    void Update(){
+        isPressed = !button.transform.GetChild(0).gameObject.activeInHierarchy;
+        Debug.Log(isPressed);
+        if(isPressed){
+            gameObject.GetComponent<BoxCollider2D>().enabled = false;
+            gameObject.GetComponent<SpriteRenderer>().enabled = false;
+        }
+        else{
+            gameObject.GetComponent<BoxCollider2D>().enabled = true;
+            gameObject.GetComponent<SpriteRenderer>().enabled = true;
+        }
+        
+    }
+}
---
+++ Game Jam 2023/Assets/ButtonPress.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonPress : MonoBehaviour
+{
+    public GameObject button;
+    private float _ButtonRisedelay;
+    private bool onButton;
+    // Start is called before the first frame update
+    void OnTriggerEnter2D(Collider2D col){
+        if(col.gameObject.tag == "Tall" || col.gameObject.tag == "Short"){
+            button.SetActive(false);
+            _ButtonRisedelay = 5f;
+            onButton = true;
+        }
+    }
+    void OnTriggerExit2D(Collider2D col){
+        onButton = false;
+        
+    }
+    void Update(){
+        if(onButton == false){
+            _ButtonRisedelay -= Time.deltaTime;
+            if(_ButtonRisedelay <= 0f){
+                button.SetActive(true);
+            }
+        }
+    }
+
+}
