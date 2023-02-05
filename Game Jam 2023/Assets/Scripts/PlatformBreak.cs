e384e11a0208f076cdf6fc1eee1fe21ec2a10a01
Michael <michaelgarcia5656@gmail.com>
2023-02-05 02:27:06 -0500

    platform works

--- Game Jam 2023/Assets/Scripts/PlatformBreak.cs
+++ Game Jam 2023/Assets/Scripts/PlatformBreak.cs
@@ -4,21 +4,25 @@ using UnityEngine;
 
 public class PlatformBreak : MonoBehaviour
 {
+    public Animator platformBreakAnim;
     public float TimeTillRespawn = 3f;
     void OnCollisionEnter2D(Collision2D col){
         GameObject player = GameObject.Find("Player");
         float yVelocity = player.GetComponent<Rigidbody2D>().velocity.y;
-        if(col.gameObject.tag == "Tall" && yVelocity == 0){
-            this.gameObject.GetComponent<SpriteRenderer>().enabled = false;
+        if(col.gameObject.tag == "Tall" && yVelocity <= 0.01){
+            platformBreakAnim.SetBool("Broke", true);
             this.gameObject.GetComponent<BoxCollider2D>().enabled = false;
             StartCoroutine(RespawnPlatform());
         }
     }
 
     IEnumerator RespawnPlatform(){
+        yield return new WaitForSeconds(0.2f);
+        this.gameObject.GetComponent<SpriteRenderer>().enabled = false;
         yield return new WaitForSeconds(TimeTillRespawn);
         this.gameObject.GetComponent<SpriteRenderer>().enabled = true;
         this.gameObject.GetComponent<BoxCollider2D>().enabled = true;
+        platformBreakAnim.SetBool("Broke", false);
     }
 
 }
