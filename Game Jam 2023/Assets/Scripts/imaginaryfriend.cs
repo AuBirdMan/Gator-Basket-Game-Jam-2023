0585e6f035302a7a11f929fb6d537c5ddcb1de9e
RatthewThePlagueBringer <matthewgrimaldi111@gmail.com>
2023-02-04 19:16:12 -0500

    Grapple foundation

--- Game Jam 2023/Assets/Scripts/imaginaryfriend.cs
+++ Game Jam 2023/Assets/Scripts/imaginaryfriend.cs
@@ -13,6 +13,8 @@ public class imaginaryfriend : MonoBehaviour
     private Renderer rend;
     private Vector2 wispGoal;
     [SerializeField] private float wispSpeed;
+    private bool cursorControl;
+    private Vector2 cursor_pos;
 
     // Start is called before the first frame update
     void Start(){
@@ -21,28 +23,49 @@ public class imaginaryfriend : MonoBehaviour
         rend.enabled = false;
         wisp = GetComponent<Rigidbody2D>();
         player_rb = player_go.GetComponent<Rigidbody2D>();
+        cursorControl = false;
 
     }
 
     // Update is called once per frame
     void Update()
     {
-
+        //Sets wisp to be invisible when player is adult
         if (player_go.transform.GetChild(0).gameObject.activeSelf){rend.enabled = true;}
         else {rend.enabled = false;}
 
         player_pos = player_go.transform.position;
         
-        if (player_rb.velocity.x > 0)
-        {
-            wispGoal = new Vector2(player_pos.x - 1f, player_pos.y + 2f);
-        }
-        else if (player_rb.velocity.x < 0)
-        {
-            wispGoal = new Vector2(player_pos.x + 1f, player_pos.y + 2f);
-        }
-        else { wispGoal = new Vector2(player_pos.x, player_pos.y + 2.5f); }
 
+        //Sets the wisp's goal to be above and behind the player when it isn't going towards the cursor
+        if (!cursorControl)
+        {
+            if (player_rb.velocity.x > 0)
+            {
+                wispGoal = new Vector2(player_pos.x - 1f, player_pos.y + 2f);
+            }
+            else if (player_rb.velocity.x < 0)
+            {
+                wispGoal = new Vector2(player_pos.x + 1f, player_pos.y + 2f);
+            }
+            else { wispGoal = new Vector2(player_pos.x, player_pos.y + 2.5f); }
+        }
+
+        //"Fires" the wisp towards the cursor, aka sets the goal to be the cursor
+        if (Input.GetKeyDown(KeyCode.Q))
+        {
+            cursorControl = true;
+            cursor_pos = Input.mousePosition;
+            wispGoal = cursor_pos;
+        }
+
+
+        if (Input.GetKeyDown(KeyCode.P))
+        {
+            cursorControl = false;
+        }
+
+        //Makes the wisp move towards its goal
         if (wisp.position.x < wispGoal.x)
         {
             wisp.velocity = new Vector2(wispSpeed, wisp.velocity.y);
@@ -55,7 +78,6 @@ public class imaginaryfriend : MonoBehaviour
         {
             wisp.velocity = new Vector2(0f, wisp.velocity.y);
         }
-
         if (wisp.position.y < wispGoal.y)
         {
             wisp.velocity = new Vector2(wisp.velocity.x, wispSpeed);
@@ -68,5 +90,8 @@ public class imaginaryfriend : MonoBehaviour
         { 
             wisp.velocity = new Vector2(wisp.velocity.x, 0f); 
         }
+
+        
+
     }
 }
