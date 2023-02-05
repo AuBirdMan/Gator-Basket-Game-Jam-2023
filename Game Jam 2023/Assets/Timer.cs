ac53cc8437561dcc98c1a8ba6ff22d4af723674c
Michael <michaelgarcia5656@gmail.com>
2023-02-05 08:08:46 -0500

    Whole buncha stuff

---
+++ Game Jam 2023/Assets/Timer.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class Timer : MonoBehaviour
+{
+    public TextMeshProUGUI timeText;
+    private float time = 0;
+    // Start is called before the first frame update
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        time += Time.deltaTime;
+        timeText.text = ((int)time).ToString();
+
+    }
+}
