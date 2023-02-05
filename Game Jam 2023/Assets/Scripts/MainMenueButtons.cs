218b5b80cfafb71ace24365c3b6b23e964acb712
Michael <michaelgarcia5656@gmail.com>
2023-02-05 04:36:51 -0500

    menuescript fix

--- Game Jam 2023/Assets/Scripts/MainMenueButtons.cs
+++ Game Jam 2023/Assets/Scripts/MainMenueButtons.cs
@@ -6,22 +6,41 @@ using UnityEngine.UI;
 
 public class MainMenueButtons : MonoBehaviour
 {
-    private Panel credits[];
+    private GameObject[] panels;
     void Start()
     {
-        Panel credits[] = FindObjectsOfType<Panel>;
+        panels = FindObjectsOfType<GameObject>();
     }
 
     public void StartGame(){
         SceneManager.LoadScene("Back2Basics");
     }
     public void Option(){
-
+        foreach (var panel in panels)
+        {
+            if(panel.name == "Options"){
+                panel.SetActive(true);
+            }
+        }
     }
     public void QuitGame(){
         Application.Quit();
     }
     public void Credits(){
-        credits.enabled=true;
+        foreach (var panel in panels)
+        {
+            if(panel.name == "Credits"){
+                panel.SetActive(true);
+            }
+        }
+    }
+
+    public void Back(){
+        foreach (var panel in panels)
+        {
+            if(panel.name == "Options" || panel.name == "Credits"){
+                panel.SetActive(true);
+            }
+        }
     }
 }
